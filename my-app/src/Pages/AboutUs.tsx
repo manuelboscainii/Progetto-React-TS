ed4f52c05d8f4a8283ba797cbe1139f2fdf95c27
manuelboscainii <boscamanu05@gmail.com>
2026-01-20 14:04:10 +0100

    Creazione pagina AboutUs.tsx

---
+++ my-app/src/Pages/AboutUs.tsx
@@ -0,0 +1,14 @@
+export default function AboutUs() {
+
+    return (
+        <>
+            <h2>About Us</h2>
+            <h4>Siamo nati nel 2020 con l'intenzione di trovare casa a piccole creature che sono rimaste orfane
+                o che sono state meno fortunate di altre. <br/>
+                Da noi puoi trovare il tuo piccolo amichetto da compagnia a 4 zampe.
+                Dai più stravaganti ai più tranquilli, da quelli monocolore a quelli di diversi colori! </h4>
+            <img src={"/src/images/foto2.jpeg"} alt={"foto gatti"} id={"foto2"}/>
+            <h4>Loro sono Nerone e Batuffolo, due fratelli inseparabili, i primi orfanelli che sono arrivati in questo centro adozione.
+                Fortunatamente per loro, hanno già trovato un tetto e una famiglia che li accudisce nel migliore dei modi 😻</h4>
+        </>);
+}
