8304fefd0b7beab6f9cc4be82df777e0a94c020c
manuelboscainii <boscamanu05@gmail.com>
2026-01-20 16:57:40 +0100

    Implementato PageNotFound in App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from "./Pages/Home.tsx";
+import AboutUs from "./Pages/AboutUs.tsx";
+import ListaGatti from "./Pages/ListaGatti.tsx";
+import Contact from "./Pages/Contacts.tsx";
+import Adozione from "./Pages/Adotta.tsx";
+import PageNotFound from "./Pages/404.tsx";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const client = new QueryClient({
+    defaultOptions: {
+        queries:
+            {
+                retryDelay: 1000,
+                retry: 3,
+            }
+    }
+});
+
+function App(){
+
+    return(
+        <>
+            <QueryClientProvider client = {client}>
+
+            <h1>🐱 ADOZIONE GATTI 🐱</h1>
+
+            <BrowserRouter>
+                <nav>
+                    <Link to ='/home'>Home</Link>
+                    <Link to ='/listagatti'>Lista Gatti</Link>
+                    <Link to ='/aboutus'>About Us</Link>
+                    <Link to ='/contact'>Contacts</Link>
+                    <Link to ='/adotta'>Adotta</Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/listagatti" element={<ListaGatti />} />
+                    <Route path="/aboutus" element={<AboutUs />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/adotta" element={<Adozione />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+
+            </BrowserRouter>
+            </QueryClientProvider>
+
+        </>);
 }
 
-export default App
+export default App;
