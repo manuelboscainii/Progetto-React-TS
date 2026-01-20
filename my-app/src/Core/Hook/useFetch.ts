36dd6ee911cb75f18333f86a1f9da70f87192310
manuelboscainii <boscamanu05@gmail.com>
2026-01-20 13:58:34 +0100

    Creazione pagina useFetch.ts

---
+++ my-app/src/Core/Hook/useFetch.ts
@@ -0,0 +1,55 @@
+
+import { useEffect, useState } from "react";
+
+/**
+ * In: url: string
+ * OUT:
+ *      - dati
+ *      - loading
+ *      - error
+ */
+
+/**
+ * 
+ * Custom hook per il fetch dei dati da un URL
+ * @param url - l'URL da cui effettuare il fetch
+ * @returns Un oggetto contenente i dati, lo stato di loading e l'eventuale errore.
+ */
+
+export default function useFetch<T>(url: string) {
+
+    // DATI
+    const [data, setData] = useState<T>(); // T = generics
+
+    // ERRORI
+    const [error, setError] = useState<string>();
+
+    // LOADING
+    const [loading, setLoading] = useState<boolean>();
+
+    async function getData() {
+        setLoading(true);
+        try {
+            const res = await fetch(url);
+
+            if (!res.ok) {
+                throw new Error("Si è verificato un errore nel recupero dei dati");
+            }
+
+            const data: T = await res.json();
+
+            setData(data);
+
+        } catch (err: any) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return {data, loading, error}
+}
