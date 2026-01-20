eb16e9e80ca284a192cd53436ed39926baeea450
manuelboscainii <boscamanu05@gmail.com>
2026-01-20 12:29:46 +0100

    Modifica alla pagina Home.tsx

---
+++ my-app/src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+
+    return (
+        <>
+            <h2>Home Page</h2>
+            <h4>Qui ci sono baffi curiosi, fusa infinite e gatti pronti a trovare finalmente casa. <br/>
+                Ogni adozione è un nuovo inizio: per loro, ma soprattutto per te.</h4>
+            <img src={"/src/images/foto3.jpeg"} alt={"foto gatto"} id={"foto3"}/>
+        </>
+    );
+
+}
