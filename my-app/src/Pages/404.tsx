c42a571898765cbb4387cd83159e4f5a8dfdc873
manuelboscainii <boscamanu05@gmail.com>
2026-01-22 11:16:28 +0100

    Documentazione pagina 404.tsx

--- my-app/src/Pages/404.tsx
+++ my-app/src/Pages/404.tsx
@@ -1,12 +1,17 @@
+// Pagina 404.tsx che mostra un messaggio di errore e un pulsante per tornare alla home.
+// Se l'url inserito non dovesse corrispondere a nessuna pagina esistente, l'utente verrà reindirizzato a questa pagina.
 
 import { useNavigate } from "react-router-dom";
 
 export default function PageNotFound() {
+    
+    // Hook per la navigazione tra le pagine
     const nav = useNavigate();
 
     return (
         <>
-            <h1>Pagina non trovata</h1>
+            <h3>Errore 404: La pagina che stai cercando non esiste.</h3>
+            {/* Pulsante per tornare alla home */}
             <button onClick={() => nav('/home')}> Torna alla Home </button>
         </>
     );
