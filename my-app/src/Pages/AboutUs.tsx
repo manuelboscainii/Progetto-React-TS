53c8e79b4fc173a17f0c23ed8273699188918448
manuelboscainii <boscamanu05@gmail.com>
2026-01-22 10:15:02 +0100

    Documentazione pagina AboutUs.tsx

--- my-app/src/Pages/AboutUs.tsx
+++ my-app/src/Pages/AboutUs.tsx
@@ -1,3 +1,6 @@
+// Creazione della pagina AboutUs.tsx che descrive la missione del sito di adozione gatti. 
+// Contiene un titolo, un paragrafo descrittivo e un'immagine di due gatti adottati.
+
 export default function AboutUs() {
 
     return (
@@ -7,7 +10,7 @@ export default function AboutUs() {
                 o che sono state meno fortunate di altre. <br/>
                 Da noi puoi trovare il tuo piccolo amichetto da compagnia a 4 zampe.
                 Dai più stravaganti ai più tranquilli, da quelli monocolore a quelli di diversi colori! </h4>
-            <img src={"/src/images/foto2.jpeg"} alt={"foto gatti"} id={"foto2"}/>
+            <img src={"/src/images/foto2.jpeg"} alt={"foto.gatti"} id={"foto2"}/>
             <h4>Loro sono Nerone e Batuffolo, due fratelli inseparabili, i primi orfanelli che sono arrivati in questo centro adozione.
                 Fortunatamente per loro, hanno già trovato un tetto e una famiglia che li accudisce nel migliore dei modi 😻</h4>
         </>);
