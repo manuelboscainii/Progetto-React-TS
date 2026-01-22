0df3f3bcc51abbf66844f6f022ea3388dc7b36a2
manuelboscainii <boscamanu05@gmail.com>
2026-01-22 11:14:26 +0100

    Documentazione pagina Home.tsx

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -1,3 +1,5 @@
+// Pagina Home.tsx che contiene un titolo, una descrizione e un'immagine di un gatto. 
+// La pagina accoglie gli utenti e li invita a esplorare il sito per trovare il loro nuovo amico a quattro zampe.
 
 export default function Home() {
 
@@ -6,6 +8,7 @@ export default function Home() {
             <h2>Home Page</h2>
             <h4>Qui ci sono baffi curiosi, fusa infinite e gatti pronti a trovare finalmente casa. <br/>
                 Ogni adozione è un nuovo inizio: per loro, ma soprattutto per te.</h4>
+            {/* Immagine di un gatto per rendere la pagina più accogliente */}
             <img src={"/src/images/foto3.jpeg"} alt={"foto gatto"} id={"foto3"}/>
         </>
     );
