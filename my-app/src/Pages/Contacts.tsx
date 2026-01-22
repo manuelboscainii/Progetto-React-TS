4bef272ada65837e8c37540478a2b122a8952fc1
manuelboscainii <boscamanu05@gmail.com>
2026-01-22 11:30:29 +0100

    Documentazione pagina Contacts.tsx

--- my-app/src/Pages/Contacts.tsx
+++ my-app/src/Pages/Contacts.tsx
@@ -1,9 +1,11 @@
+// Pagina Contacts.tsx che fornisce le informazioni di contatto dell'organizzazione.
+
 export default function Contacts() {
 
     return (
         <>
             <h2>Contatti</h2>
-            <h4>Ci trovate in Via delle 9 Vite a Verona! </h4>
+            <h4>Ci trovate in Via delle 7 Vite a Verona! </h4>
             <h4>Seguiteci anche su Instagram e Facebook - miagolandoo.it </h4>
             <h4>📞 +39 345 3214 789 </h4>
             <h4>✉️ miagolandoo@gmail.com </h4>
